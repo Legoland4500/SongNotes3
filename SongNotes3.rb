8c8fb9c40780531dd4ee5c999350b345df64c0c8
Legoland <99356387+Legoland4500@users.noreply.github.com>
2022-03-31 11:30:10 -0700

    Create SongNotes3.rb

---
+++ SongNotes3.rb
@@ -0,0 +1,45 @@
+use_bpm 120
+use_synth :dsaw
+
+live_loop :wow do
+  3.times do
+    play :g4
+    play :g3
+    sleep 1
+    play :g4
+    play :g3
+    sleep 1/2.0
+    play :g4
+    play :g3
+    sleep 1/4.0
+    play :g4
+    play :g3
+    sleep 1/2.0
+    play :g4
+    play :g3
+    sleep 1/4.0
+    play :g4
+    play :g3
+    sleep 1/2.0
+    play :a4
+    play :a3
+    sleep 1/2.0
+    play :b4
+    play :b3
+    sleep 1/2.0
+  end
+  1.times do
+    play :c5
+    play :c4
+    sleep 1
+    play :b4
+    play :b3
+    sleep 1
+    play :a4
+    play :a3
+    sleep 1
+    play :b4
+    play :b3
+    sleep 1
+  end
+end
